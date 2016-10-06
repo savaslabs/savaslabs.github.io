1a6684c3313310cced9ceafe2ab53039aeab4d07
Kosta Harlan <kosta@kostaharlan.net>
2016-10-06 16:29:04 -0400

    another test

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -3,38 +3,34 @@
 # Enable error reporting to the console.
 set -e
 
-curl https://www.teleconsole.com/get.sh | sh
-teleconsole
-sleep 60
+# curl https://www.teleconsole.com/get.sh | sh
+# teleconsole
+# sleep 60
 
 # Install bundles if needed
 bundle check || bundle install
 
 # NPM install if needed.
+nvm use 6.1
 npm install
 
 # Build the site.
 gulp
 
-ls -la _site
-ls -la /tmp
-
-# Stash built site.
-cp -a _site /tmp/
-cp .travis.yml /tmp/.travis.yml
-
 # Checkout master and remove everything
-git config user.email ${GH_EMAIL}
-git config user.name "savas-bot"
+git clone https://${GH_TOKEN}@github.com/savaslabs/savaslabs.github.io.git ../savaslabs.github.io.master
+cd ../savaslabs.github.io.master
 git checkout master
 rm -rf *
 
 # Copy generated HTML site from source branch in original repo.
 # Now the master branch will contain only the contents of the _site directory.
-cp -R /tmp/_site/* .
+cp -R ../savaslabs.github.io/_site/* .
 
 # Make sure we have the updated .travis.yml file so tests won't run on master.
-cp /tmp/.travis.yml .
+cp ../savaslabs.github.io/.travis.yml .
+git config user.email ${GH_EMAIL}
+git config user.name "savas-bot"
 
 # Commit and push generated content to master branch.
 git status
