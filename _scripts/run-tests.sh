3cbba0cc209ec213339de14b29a3f8435b65d3df
Chris Russo <chris.andrews.russo@gmail.com>
2017-04-21 15:56:07 -0400

    Add note to test script to speed post testing

--- _scripts/run-tests.sh
+++ _scripts/run-tests.sh
@@ -2,5 +2,7 @@
 set -e
 gulp build:test
 bundle exec scss-lint _assets/styles/scss/*/*.scss
+# Run markdown lint
+# bundle exec mdl _posts/2017-04-20-web-redesign.md -c .mdlrc -g
 bundle exec mdl . -c .mdlrc --git-recurse
 bundle exec rake test -f Rakefile
