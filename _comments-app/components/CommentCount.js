a6458c8516930bbe60d74f46b9045212b343468e
Kosta Harlan <kosta@fastmail.com>
2017-10-30 10:29:12 -0400

    Set comment count label depending on count

--- _comments-app/components/CommentCount.js
+++ _comments-app/components/CommentCount.js
@@ -18,10 +18,11 @@ class CommentCount extends Component {
   }
   render () {
     if (this.state.count > 0) {
+      const label = this.state.count > 1 ? 'comments' : 'comment';
       return (
         <a href="#js-comments">
           <i className="fa fa-comment"></i>
-          {this.state.count} comments
+          {this.state.count} {label}
         </a>
       );
     }
