75e9258dc3db39dd402686940e8cccff8dd3038f
Anne Tomasevich <annetvich@gmail.com>
2017-12-06 15:53:58 -0500

    Issue #4956: Update test scripts to decrease testing time

--- _scripts/run-tests.sh
+++ _scripts/run-tests.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
-node_modules/eslint/bin/eslint.js _comments-app/
-bundle exec scss-lint _assets/styles/scss/*/*.scss
-bundle exec mdl . -c .mdlrc --git-recurse
+
+if [ -z "$TRAVIS" ]; then
+  codeclimate analyze
+fi
 gulp build:test
-gulp accessibility-test
 bundle exec rake test -f Rakefile
+gulp accessibility-test
