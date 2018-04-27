3a96dde384fa243df5d60264a10a268075981666
Kosta Harlan <kosta@fastmail.com>
2018-04-27 09:54:22 -0400

    Issue #5327: Allocate more RAM to gulp

--- _scripts/run-tests.sh
+++ _scripts/run-tests.sh
@@ -10,5 +10,5 @@ bundle exec rake test -f Rakefile
 # Since this test takes a while, only run it on feature branches.
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [[ "$BRANCH" != "source" ]]; then
-  gulp accessibility-test
+  node --max-old-space-size=6000 ./node_modules/.bin/gulp accessibility-test
 fi
